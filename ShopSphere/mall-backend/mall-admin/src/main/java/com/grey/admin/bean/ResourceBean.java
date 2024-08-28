5e92fe5dfa1388e76ee5ab6eacbb93b5e222e215
grey <greyjing.zhou@ncsi.com.cn>
2024-08-28 16:17:17 +0800

    update

---
+++ ShopSphere/mall-backend/mall-admin/src/main/java/com/grey/admin/bean/ResourceBean.java
@@ -0,0 +1,27 @@
+package com.grey.admin.bean;
+
+
+import lombok.Data;
+
+/**
+ * Spring Security中，资源被简化成一个字符串。
+ * 而在自己设计资源时，可以设计不同类型的资源控制不同的行为。
+ * 例如 菜单资源，Rest接口资源，页面控件资源等。
+ */
+@Data
+public class ResourceBean {
+
+    private String resourceId;
+    private String resourceType;
+    private String resourceName;
+
+    public ResourceBean(){
+
+    }
+
+    public ResourceBean(String resourceId, String resourceName) {
+        this.resourceId = resourceId;
+        this.resourceName = resourceName;
+    }
+
+}
