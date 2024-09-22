0df4625c1a39bfa1ae7e364e320c15d0980bd0b8
gugibv <gugibv@163.com>
2024-09-22 19:32:31 +0800

    update

--- ShopSphere/mall-frontend/mall-admin-web/vue.config.js
+++ ShopSphere/mall-frontend/mall-admin-web/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8081,   // 前端运行在 8081 端口
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 代理到后端运行的地址
+        changeOrigin: true,
+        pathRewrite: { '^/api': '/api' } // 将 /api 去掉，直接转发请求
+      }
+    }
+  }
 })
