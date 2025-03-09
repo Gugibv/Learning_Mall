07e3c363b8d9353eb0babbd222006698a72926f7
gugibv <gugibv@163.com>
2025-03-09 08:28:37 +0800

    add rabbit mq

--- ShopSphere/mall-backend/mall-admin/src/main/java/com/macro/study/test/rabbitmq/RabbitMQUtil.java
+++ ShopSphere/mall-backend/mall-admin/src/main/java/com/macro/study/test/rabbitmq/RabbitMQUtil.java
@@ -6,12 +6,12 @@ import com.rabbitmq.client.ConnectionFactory;
 public class RabbitMQUtil {
 
     private static Connection connection;
-    public static final String HOST_NAME="172.31.2.200";
+    public static final String HOST_NAME="127.0.0.1";
     public static final int HOST_PORT=5672;
 
-    public static final String USER_NAME="vade";
-    public static final String PASSWORD="vade123456";
-    public static final String VIRTUAL_HOST="/grey";
+    public static final String USER_NAME="admin";
+    public static final String PASSWORD="admin";
+    public static final String VIRTUAL_HOST="grey";
 
     public static final String QUEUE_NAME="test";
     public static final String EXCHANGE_NAME="myExchange";
